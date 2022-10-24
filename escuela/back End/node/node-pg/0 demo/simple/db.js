608830e6b03bb6fa00c8b5eb38b5f886ad313e2f
Denny Sanchez <d3nnysm2904@LAPTOP-TUKF1SRM.localdomain>
2022-10-23 21:28:45 -0400

    2

---
+++ escuela/back End/node/node-pg/0 demo/simple/db.js
@@ -0,0 +1,21 @@
+/** Database setup for users. */
+
+const { Client } = require( "pg" );
+
+let DB_URI;
+
+if ( process.env.NODE_ENV === "test" )
+{
+  DB_URI = "postgresql:///users_test";
+} else
+{
+  DB_URI = "postgresql:///usersdb";
+}
+
+let db = new Client( {
+  connectionString: DB_URI
+} );
+
+db.connect();
+
+module.exports = db;
